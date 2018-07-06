18fb63371ca325ee4199aa2d24835934123c42e2
Thuwarakesh <thuwarakesh@gmail.com>
2018-07-06 23:12:32 +0530

    login formed (unstyled) created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -16,9 +22,14 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
-    )
+    ),
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.username);
+    console.log(this.password);
   }
 
 }
