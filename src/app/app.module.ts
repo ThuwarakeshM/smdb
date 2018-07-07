8b05ac6e226eac9e4242b281ef661fddd86f8c77
Thuwarakesh <thuwarakesh@gmail.com>
2018-07-07 09:47:52 +0530

    new pages components added to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
-import {CardModule} from 'primeng/card';
-import {InputTextModule} from 'primeng/inputtext';
-import {PasswordModule} from 'primeng/password';
-import {ButtonModule} from 'primeng/button';
-import {ToolbarModule} from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -19,9 +19,11 @@ import { ThemesComponent } from './themes/themes.component';
 import { ForecastsComponent } from './forecasts/forecasts.component';
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'home', component: HomeComponent},
-
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'Highlights', component: HighlightsComponent },
+  { path: 'Themes', component: ThemesComponent },
+  { path: 'Forecasts', component: ForecastsComponent },
 ];
 
 @NgModule({
